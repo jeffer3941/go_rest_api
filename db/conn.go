50bfa3c4de24bbf496cbefcb45dd81a14aa99c8b
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2024-08-10 15:03:53 -0300

    feat: criando conexao com o banco de dados

---
+++ db/conn.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+	sslmode  = "disable"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to " + dbname)
+
+	return db, nil
+}
