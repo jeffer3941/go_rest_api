487d8a59e8911fe36953bfbfe70d21c57b01bcd4
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2024-08-17 13:56:14 -0300

    fix:  alterando dados

--- model/drinks.go
+++ model/drinks.go
@@ -1,8 +1,8 @@
 package model
 
 type Drinks struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Value       string `json:"Value"`
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Value       float32 `json:"value"`
 }
