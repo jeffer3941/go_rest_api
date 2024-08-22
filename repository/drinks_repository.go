b57151e1715f4d43b423a84269fab0e0b45d2e3b
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2024-08-22 14:11:00 -0300

    feat: alterando repository para o gorm

--- repository/drinks_repository.go
+++ repository/drinks_repository.go
@@ -1,82 +1,77 @@
 package repository
 
 import (
-	"database/sql"
 	"drink-api/model"
 	"fmt"
+
+	"gorm.io/gorm"
 )
 
 type DrinksRepository struct {
-	connection *sql.DB
+	connection *gorm.DB
 }
 
-func NewDrinksRepository(connection *sql.DB) DrinksRepository {
+func NewDrinksRepository(connection *gorm.DB) DrinksRepository {
 	return DrinksRepository{
 		connection: connection,
 	}
 }
 
 func (dr *DrinksRepository) GetDrinks() ([]model.Drinks, error) {
-	query := "SELECT id, name, description, value FROM drinks"
-	rows, err := dr.connection.Query(query)
-	if err != nil {
+	var drinks []model.Drinks
+
+	if err := dr.connection.Find(&drinks).Error; err != nil {
 		fmt.Println(err)
 		return []model.Drinks{}, err
-	}
-
-	var drinks []model.Drinks
-	var DrinkObj model.Drinks
-
-	for rows.Next() {
-		err := rows.Scan(&DrinkObj.Id, &DrinkObj.Name, &DrinkObj.Description, &DrinkObj.Value)
-		if err != nil {
-			fmt.Println(err)
-			return []model.Drinks{}, err
-		}
-		drinks = append(drinks, DrinkObj)
 	}
 
 	return drinks, nil
 }
 
 func (dr *DrinksRepository) GetDrinksById(id int) (model.Drinks, error) {
-	query := "SELECT id, name, description, value FROM drinks WHERE id = $1"
-	row := dr.connection.QueryRow(query, id)
 	var drinks model.Drinks
-	err := row.Scan(&drinks.Id, &drinks.Name, &drinks.Description, &drinks.Value)
-	if err != nil {
+
+	if err := dr.connection.First(&drinks, id).Error; err != nil {
 		fmt.Println(err)
 		return model.Drinks{}, err
 	}
+
 	return drinks, nil
 }
 
 func (dr *DrinksRepository) CreateDrinks(drink model.Drinks) (model.Drinks, error) {
-	query := "INSERT INTO drinks (name, description, value) VALUES ($1, $2, $3) RETURNING id"
-	err := dr.connection.QueryRow(query, drink.Name, drink.Description, drink.Value).Scan(&drink.Id)
-	if err != nil {
-		fmt.Println(err)
-		return model.Drinks{}, err
+	result := dr.connection.Create(&drink)
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		return model.Drinks{}, result.Error
 	}
 	return drink, nil
 }
 
-func (dr *DrinksRepository) UpdateDrinks(id int, drink model.Drinks) (model.Drinks, error) {
-	query := "UPDATE drinks SET name = $1, description = $2, value = $3 WHERE id = $4"
-	_, err := dr.connection.Exec(query, drink.Name, drink.Description, drink.Value, id)
-	if err != nil {
+func (dr *DrinksRepository) UpdateDrinks(id int, updateDrink model.Drinks) (model.Drinks, error) {
+	var drink model.Drinks
+
+	if err := dr.connection.First(&drink, id).Error; err != nil {
 		fmt.Println(err)
 		return model.Drinks{}, err
 	}
+
+	drink.Name = updateDrink.Name
+	drink.Description = updateDrink.Description
+	drink.Value = updateDrink.Value
+
+	if err := dr.connection.Save(&drink).Error; err != nil {
+		fmt.Println(err)
+		return model.Drinks{}, err
+	}
+
 	return drink, nil
 }
 
 func (dr *DrinksRepository) DeleteDrinks(id int) error {
-	query := "DELETE FROM drinks WHERE id = $1"
-	_, err := dr.connection.Exec(query, id)
-	if err != nil {
-		fmt.Println(err)
-		return err
+	if er := dr.connection.Delete(&model.Drinks{}, id); er.Error != nil {
+		fmt.Println(er.Error)
+		return er.Error
 	}
 	return nil
 }
